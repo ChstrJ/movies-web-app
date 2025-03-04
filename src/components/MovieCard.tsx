3acb88a3eb5f6996d125a3140e7d7c239ddde6ad
root <chestervjazmin@gmail.com>
2025-03-04 02:46:32 +0000

    add api client

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,22 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+
+const MovieCard = ({ img }) => {
+  return (
+    <Card>
+
+
+    </Card>
+  )
+};
+
+
+export default MovieCard;
+
