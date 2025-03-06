9fd6fcd4032fc3f98259546104b65f825bbfe3f9
ChstrJ <chestervjazmin@gmail.com>
2025-03-06 20:13:53 +0800

    miodify title

--- src/components/CustomNavbar.tsx
+++ src/components/CustomNavbar.tsx
@@ -2,21 +2,29 @@ import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/react";
 import { Link } from "react-router-dom";
 import MovieIcon from "./MovieIcon";
 
-
-
 export default function CustomNavbar() {
   return (
     <Navbar className="backdrop-filter backdrop-blur-lg bg-opacity-30 p-4">
       <NavbarBrand>
-        <MovieIcon className='w-6 mr-2' />
-        <p className="font-bold text-xl text-white">Netflix & Chill</p>
+        <MovieIcon className="w-6 mr-2" />
+        <p className="font-bold text-xl text-white">Netpleks & Chill</p>
       </NavbarBrand>
       <NavbarContent className="gap-4" justify="center">
         <NavbarItem className="hidden sm:flex">
-          <Link to={'/movies'} className="text-sm p-2 text-white hover:text-gray-400 duration-300 ease-in-out">Movies</Link>
+          <Link
+            to={"/movies"}
+            className="text-sm p-2 text-white hover:text-gray-400 duration-300 ease-in-out"
+          >
+            Movies
+          </Link>
         </NavbarItem>
         <NavbarItem className="hidden sm:flex">
-          <Link to={'/shows'} className="text-sm p-2 text-white rounded-md hover:text-gray-400 duration-200 ease-in-out">Shows</Link>
+          <Link
+            to={"/shows"}
+            className="text-sm p-2 text-white rounded-md hover:text-gray-400 duration-200 ease-in-out"
+          >
+            Shows
+          </Link>
         </NavbarItem>
         {/* <NavbarItem className="hidden sm:flex">
           <Input
@@ -26,9 +34,7 @@ export default function CustomNavbar() {
             value={''}
           />
         </NavbarItem> */}
-
       </NavbarContent>
     </Navbar>
   );
 }
-
