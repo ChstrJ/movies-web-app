263e869c3cb1dd3364249e0c2a422ffe35f0abc2
root <chestervjazmin@gmail.com>
2025-03-07 06:57:50 +0000

    add badge

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import { Command, CommandEmpty, CommandGroup, CommandItem, CommandList } from "./ui/command";
+import { Badge } from "./ui/badge";
+import { cn } from "@/lib/utils";
 
 type SearchResultItem = {
   id: number;
@@ -21,7 +23,11 @@ const SearchResult = ({ results }: SearchResultProps) => {
             ? <CommandEmpty>No results found.</CommandEmpty>
             : results.map(result => (
               <Link to={`/movies/{result.id}`} key={result.id} >
-                <CommandItem className="cursor-pointer hover:opacity-50" >{result.title || result.name}</CommandItem>
+                <CommandItem className="cursor-pointer hover:text-slate-400 p-2">{result.title || result.name}
+                  {result.name
+                    ? <Badge variant={"outline"}>Series</Badge>
+                    : <Badge variant={"outline"}>Movie</Badge>}
+                </CommandItem>
               </Link>
             ))
           }
