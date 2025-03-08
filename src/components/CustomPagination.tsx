228dfd776d25b968a63419103734c5177a8ebd0b
Chester <89991461+ChstrJ@users.noreply.github.com>
2025-03-08 13:47:43 +0800

    Fix pagination

--- src/components/CustomPagination.tsx
+++ src/components/CustomPagination.tsx
@@ -10,14 +10,18 @@ import { usePaginationStore } from "@/stores/usePaginationStore"
 export function CustomPagination() {
   const { page, setPage } = usePaginationStore();
 
+  console.log(page)
+
   return (
     <Pagination>
       <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious
-            onClick={() => setPage(page - 5)}
-            className="text-white hover:text-gray-400 duration-300 ease-in-out" href="#" />
-        </PaginationItem>
+        {page > 5 && (
+          <PaginationItem>
+            <PaginationPrevious
+              onClick={() => setPage(page - 5)}
+              className="text-white hover:text-gray-400 duration-300 ease-in-out" href="#" />
+          </PaginationItem>
+        )}
         <PaginationItem>
           <PaginationNext
             onClick={() => setPage(page + 5)}
