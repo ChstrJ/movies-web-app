608ff49d8266e61d92b202746ac8df59927a5adb
root <chestervjazmin@gmail.com>
2025-04-22 05:58:32 +0000

    adjust overview

--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -53,7 +53,7 @@ const MoviePage = () => {
   ]
 
   return (
-    <div className="h-screen p-2 overflow-hidden">
+    <div className="min-h-screen p-2 overflow-hidden">
       <div className='flex flex-col'>
         {isLoading && <Loader />}
         <div className='w-full h-[750px] relative overflow-auto'>
@@ -79,16 +79,19 @@ const MoviePage = () => {
           <CustomTab data={servers} />
         </div>
         {result && (
-          <div className='flex flex-col p-2 h-full'>
-            <div className='flex flex-row items-center'>
+          <div className='flex flex-col p-2'>
+            <div className='flex flex-row'>
               <img
                 src={`${src}/w154/${result.poster_path}`}
                 alt={result.title || result.name}
               />
-              <h1 className='ml-4 text-2xl font-bold text-white'>{result?.title || result?.name}
-              </h1>
+              <div className='ml-4 flex flex-col'>
+                <h1 className='text-2xl font-bold text-white'>{result?.title || result?.name}
+                </h1>
+                <p className='text-gray-400 mt-2'>{result?.overview}</p>
+
+              </div>
             </div>
-            <p className='text-gray-400'>{result?.overview}</p>
           </div>
         )}
       </div>
